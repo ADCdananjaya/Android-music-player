b283b20701a2f55405e8a31ff7802614c3016256
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-01-25 19:35:45 +0530

    add functions to the seekbar

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.musicplayer
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.SeekBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private var mediaPlayer: MediaPlayer ?= null
+    private lateinit var seekBar: SeekBar
+    private lateinit var runnable: Runnable
+    private lateinit var handler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,10 +22,13 @@ class MainActivity : AppCompatActivity() {
         val playButton = findViewById<FloatingActionButton>(R.id.playButton)
         val pauseButton = findViewById<FloatingActionButton>(R.id.pauseButton)
         val stopButton = findViewById<FloatingActionButton>(R.id.stopButton)
+        seekBar = findViewById(R.id.seekBar)
+        handler = Handler(Looper.getMainLooper())
 
         playButton.setOnClickListener {
             if (mediaPlayer == null) {
                 mediaPlayer = MediaPlayer.create(this, R.raw.song)
+                handleSeekBar()
             }
             mediaPlayer?.start()
         }
@@ -32,6 +42,32 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer?.reset()
             mediaPlayer?.release()
             mediaPlayer = null
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
         }
+    }
+
+    private fun handleSeekBar() {
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    mediaPlayer?.seekTo(progress)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+        seekBar.max = mediaPlayer!!.duration
+        runnable = Runnable {
+            // !! -> checks not null
+            seekBar.progress = mediaPlayer!!.currentPosition
+            handler.postDelayed(runnable, 500)
+        }
+        handler.postDelayed(runnable, 500)
     }
 }
