64a7314c1691d44386d0d6d253893b58634bf6e6
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-01-25 15:11:38 +0530

    fix button error

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -4,18 +4,34 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaPlayer: MediaPlayer ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.song)
+        val playButton = findViewById<Button>(R.id.playButton)
+        val pauseButton = findViewById<FloatingActionButton>(R.id.pauseButton)
+        val stopButton = findViewById<FloatingActionButton>(R.id.stopButton)
 
-        var playButton = findViewById<Button>(R.id.playButton)
         playButton.setOnClickListener {
-            mediaPlayer.start()
+            if (mediaPlayer == null) {
+                mediaPlayer = MediaPlayer.create(this, R.raw.song)
+            }
+            mediaPlayer?.start()
+        }
+
+        pauseButton.setOnClickListener {
+            mediaPlayer?.pause()
+        }
+
+        stopButton.setOnClickListener {
+            mediaPlayer?.stop()
+            mediaPlayer?.reset()
+            mediaPlayer?.release()
+            mediaPlayer = null
         }
     }
 }
