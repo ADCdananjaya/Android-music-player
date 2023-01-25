16c2660cf0b6545044281f56fa1e4f81aaaf3f10
Chamod Dananjaya <chamoddananjaya49@gmail.com>
2023-01-25 10:22:18 +0530

    add ui design

--- app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.musicplayer
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song)
+
+        var playButton = findViewById<Button>(R.id.playButton)
+        playButton.setOnClickListener {
+            mediaPlayer.start()
+        }
     }
 }
